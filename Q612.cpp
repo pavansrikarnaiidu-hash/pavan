297a6afeee09b55c2a6ad9803899d536c9ddd9f9
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-08-31 14:37:26 +0530

    Add files via upload

---
+++ Q612.cpp
@@ -0,0 +1,23 @@
+// check whether it is positive, negative or ZERO
+#include<stdio.h>
+
+  int main() {
+	int a;
+	printf("enter a no.");
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("no. is positive");
+	}
+	else if (a<0)
+	{
+		printf("no. is negative");
+	}
+	else if (a=0)
+	{
+		printf("no. is zero");
+	}
+}
+
+	
+
