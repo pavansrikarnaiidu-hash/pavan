368298a73dcb6c55a163e57861903ac5c2a0c638
pavansrikarnaiidu-hash <pavansrikarnaiidu@gmail.com>
2025-09-03 18:36:12 +0530

    Add files via upload

---
+++ Q1  2.cpp
@@ -0,0 +1,17 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+
+int main ()
+ {
+ int a,b,sum; printf ("enter the frist number:");
+ 
+ scanf("%d",&a);
+ printf("enter the second number: ");
+ scanf("%d",&b);
+ sum=a+b;
+ printf ("sum is :%d",sum);
+ 	
+ 	
+ 	return 0;
+ }
